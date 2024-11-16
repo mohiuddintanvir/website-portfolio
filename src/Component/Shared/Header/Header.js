43a54e3415f6c79db622fb9750c427098cb7bd4a
mohiuddintanvir <mohiuddin.tanvir.mollah@gmail.com>
2024-11-16 22:14:08 +0100

    submit

--- src/Component/Shared/Header/Header.js
+++ src/Component/Shared/Header/Header.js
@@ -1,118 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./header.css";
-import { Link } from "react-router-dom";
-import { SiGmail } from "react-icons/si";
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
-import {
-  AiFillFacebook,
-  AiFillInstagram,
-  AiFillLinkedin,
-  AiOutlineUserAdd,
-  AiOutlineWhatsApp,
-  AiFillGithub,
-  AiFillDribbbleCircle
-} from "react-icons/ai";
-import { FiMenu } from "react-icons/fi";
-import { GoEyeClosed } from "react-icons/go";
-import { useState } from "react";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const [navbar, setNavbar] = useState(false);
 
+  // Scroll behavior only for large screens
+  useEffect(() => {
+    const changeNavbarColor = () => {
+      // Check if the screen is large before applying the scroll behavior
+      if (window.innerWidth >= 1024 && window.scrollY > 0) {
+        setNavbar(true);
+      } else if (window.innerWidth >= 1024 && window.scrollY === 0) {
+        setNavbar(false);
+      }
+    };
 
-const [open,setOpen]=useState(false)
+    window.addEventListener("scroll", changeNavbarColor);
 
-// mobile
-const manuLinks = [
-  {
-    logo: "home-outline",
-    link: "#/",
-    tiptool: "Home",
-  },
-  {
-    logo: "person-outline",
-    link: "#about",
-    tiptool: "ABOUT",
-  },
-  {
-    logo: "construct-outline",
-    link: "#Work",
-    tiptool: "Projects",
-  },
-  {
-    logo: "call-outline",
-    link: "#contact",
-    tiptool: "Contact",
-  },
+    return () => {
+      window.removeEventListener("scroll", changeNavbarColor);
+    };
+  }, []);
 
-  
- 
-];
-
-
-
-
-// Desktop
-  const menulist1 = [
-    <li className="hover:bg-[#EAB308] hover:rounded">
-     
-     <a href="#/">Home</a>   
-   
-    </li>,
-    <li className="hover:bg-[#EAB308] hover:rounded">
-   
-      <a href="#about">About</a>  
-      
-    </li>,
-  
-    
-    <li className="hover:bg-[#EAB308] hover:rounded">
-   
-       <a href="#Work">Work</a> 
-     
-    </li>,
-    
-    <li className="hover:bg-[#EAB308] hover:rounded ">
-    
-      <a href="#contact">Contact</a>  
-   
-    </li>,
-  ];
-
-  const [navber, setNavber] = useState(false);
-
-  // scroll function
-  useEffect(()=>{
-    const changeBackgroundcolor = () => {
-    if (window.scrollY >0) {
-      setNavber(true);
-    } else {
-      setNavber(false);
-    }
-
-    
-  };
-window.addEventListener("scroll",changeBackgroundcolor);
-return()=>{
-  window.removeEventListener("scroll",changeBackgroundcolor)
-}
-  },[])
-
-  // scrolling style set
-  const navberclass=`navbar fixed  z-[999] ${navber ? 'lg:bg-[#EAB308]  md:bg-[#EAB308]  opacity-[90%]' : ''}`
-  
-  
+  // Dynamically assign navbar classes based on state
+  const navbarClass = `navbar fixed z-[999] ${
+    navbar ? "lg:bg-[#EAB308] opacity-[90%]" : ""
+  }`;
 
   return (
-    <div className={navberclass}>
-      {/* navber  */}
-      <div className="navbar  w-screen  ">
-        {/* dropdown */}
-        <div className="dropdown h-96 lg:hidden gap-20">
+    <div className={navbarClass}>
+      {/* Navbar */}
+      <div className="navbar w-screen">
+        {/* Mobile Menu (dropdown) */}
+        <div className="dropdown lg:hidden">
           <label
             tabIndex={0}
             onClick={() => setOpen(!open)}
-            className="btn btn-ghost btn-circle duration-1000 mr-2 bg-white z-10"
+            className="btn btn-ghost btn-circle bg-white z-10"
           >
             {open ? (
               <AiOutlineClose className="text-lg" />
@@ -122,37 +49,59 @@ return()=>{
           </label>
           <ul
             tabIndex={0}
-            className={`menu menu-sm  mt-3 z-[1] p-2 shadow  h-[30rem]    duration-1000 transform translate-transform ${
+            className={`menu menu-sm mt-3 z-[1] p-2 shadow h-[30rem] duration-1000 transform ${
               open ? "translate-x-0" : "-translate-x-[150%]"
-            } gap-[2.20rem]`}
+            }`}
           >
-
-
-            {/* section logo */}
-            {manuLinks.map((menu) => (
-              <div  className="ml-3">
-                <div className="text-[#F9BE08] text-xl   cursor-pointer tooltip" data-aos="zoom-out" data-tip={menu.tiptool}>
-                  <a href={menu.link}>
-                    <ion-icon name={menu.logo}> {menu.logo}</ion-icon>
-                  </a>
-                </div>
-             
-               
-              </div>
-            ))}
-
-          
-     
+            {/* Mobile menu links */}
+            <li className="text-xl">
+              <a href="#/">Home</a>
+            </li>
+            <li className="text-xl">
+              <a href="#about">About</a>
+            </li>
+            <li className="text-xl">
+              <a href="#Work">Work</a>
+            </li>
+            <li className="text-xl">
+              <a href="#contact">Contact</a>
+            </li>
           </ul>
-          
         </div>
-{/* Large screen  */}
-        <div className={`navbar-center font-bold text-2xl  ms-40 hidden lg:flex ${navber ? "text-white":"text-black "}`}>
-        <h1 className="  font-PlayfairDisplaySC font-[700]">Mohiuddin<span className={`  ${navber ? "text-black" :"text-[#EAB308]"} `}>M.</span></h1>
+
+        {/* Large Screen Navbar */}
+        <div
+          className={`navbar-center font-bold text-2xl ms-40 hidden lg:flex ${
+            navbar ? "text-white" : "text-black"
+          }`}
+        >
+          <h1 className="font-PlayfairDisplaySC font-[700]">
+            Mohiuddin
+            <span className={` ${navbar ? "text-black" : "text-[#EAB308]"}`}>
+              M.
+            </span>
+          </h1>
         </div>
-       
-        <div className={`justify-end  lg:flex gap-3 font-PlayfairDisplaySC  font-[400]  hidden  ${navber ? "text-white": "text-black"}`}>
-           <ul className="menu ms-40 menu-horizontal font-bold text-xl">{menulist1}</ul>
+
+        <div
+          className={`justify-end lg:flex gap-3 font-PlayfairDisplaySC font-[400] hidden ${
+            navbar ? "text-white" : "text-black"
+          }`}
+        >
+          <ul className="menu ms-40 menu-horizontal font-bold text-xl">
+            <li className="hover:bg-[#EAB308] hover:rounded">
+              <a href="#/">Home</a>
+            </li>
+            <li className="hover:bg-[#EAB308] hover:rounded">
+              <a href="#about">About</a>
+            </li>
+            <li className="hover:bg-[#EAB308] hover:rounded">
+              <a href="#Work">Work</a>
+            </li>
+            <li className="hover:bg-[#EAB308] hover:rounded">
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
--- src/Component/pages/Home/Home.js
+++ src/Component/pages/Home/Home.js
@@ -1,24 +1,15 @@
 import React, { useEffect } from "react";
 import "./home.css";
-import { Link } from "react-router-dom";
-import { DiDoctrine } from "react-icons/di";
-import homeimg from "../../../image/home-img.png";
-import About from "../About/About";
-import Work from "../Work/Work";
-import Contact from "../Contact/Contact";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import Marquee from "react-fast-marquee";
 import { FaHtml5, FaCss3Alt, FaReact } from "react-icons/fa";
 import { TbBrandJavascript } from "react-icons/tb";
 import { DiNodejs } from "react-icons/di";
-import { SiMongodb, SiTailwindcss, SiBootstrap } from "react-icons/si";
-import { SiTypescript } from "react-icons/si";
-
-const socilaLink = [
-  { name: "logo-linkedin" },
-  { name: "logo-github" },
-];
+import { SiMongodb, SiTailwindcss, SiBootstrap, SiTypescript } from "react-icons/si";
+import Marquee from "react-fast-marquee";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import About from "../About/About";
+import Work from "../Work/Work";
+import Contact from "../Contact/Contact";
 
 const Home = () => {
   useEffect(() => {
@@ -27,83 +18,51 @@ const Home = () => {
 
   return (
     <>
-      <div className="bg-white h-screen w-screen flex justify-center items-center">
-        <div className="lg:flex lg:justify-center lg:items-center  h-screen w-screen lg:mt-0 mt-[20rem]" id="/">
-          <div className="box flex lg:hidden"></div>
-          <div className="lg:z-10 lg:justify-end  lg:flex lg:items-center ">
-            <div className=" lg:mt-40">
-              <div className="">
-                 <p
-                className="font-PlayfairDisplaySC font-[400] lg:font-[700] lg:text-[5rem] md:ms-10 md:font-[700] md:text-[3rem] flex justify-center lg:ml-2 "
-                data-aos="fade-up"
-                data-aos-duration="1200"
-              >
-             Mern Stack Developer
-              </p>
-              <h1
-                className="lg:text-[1.25rem] md:flex md:justify-center font-Lato lg:font-[300] lg:ms-0 ms-3 "
-                data-aos="fade-up"
-                data-aos-duration="1500"
-              >
-                Hello and welcome! I'm Mohiuddin Molla, a passionate MERN stack
-                developer based in Germany.
-              </h1>
-               <div className="">
-                <div className="" data-aos="fade-up" data-aos-duration="2000">
-                  
-                  <div className="lg:flex   lg:mt-20  mt-10 gap-3">
-                    <div className="md:ms-[13rem]">
-                      <h1 className="font-PlayfairDisplaySC text-[.75rem] lg:font-[500] md:text-xl  lg:text-[1.25rem] lg:ms-[0rem] ms-[8rem] lg:mb-[0rem] mb-3">
-                        Development Stack
-                      </h1>
-                    </div>
-                    <div className="lg:w-[40rem] md:w-[50rem] w-[23.5rem]">
-                      <Marquee>
-                        <ul className="lg:gap-5 flex text-[1rem] md:text-[3rem] md:gap-10 gap-5  lg:text-[2rem]">
-                          <li className="text-[#E5532D]">
-                            <FaHtml5 />
-                          </li>
-                          <li className="text-[#2E52E4]">
-                            <FaCss3Alt />
-                          </li>
-                          <li className="text-[#F7E025]">
-                            <TbBrandJavascript />
-                          </li>
-                          <li className="text-[#11DAFF]">
-                            <FaReact />
-                          </li>
-                          <li className="text-[#2E52E4]">
-                          <SiTypescript />
-                          </li>
-                          <li className="text-[#8FC708]">
-                            <DiNodejs />
-                          </li>
-                          <li className="text-[#0D3937]">
-                            <SiMongodb />
-                          </li>
-                          <li className="text-[#8E1AFC]">
-                            <SiBootstrap />
-                          </li>
-                          <li className="text-[#1DC0CD]">
-                            <SiTailwindcss />
-                          </li>
-                        </ul>
-                      </Marquee>
-                    </div>
-                  </div>
-                </div>
+      <div className="home-container">
+        <div className="content-wrapper">
+          {/* Animated Shape Box */}
+          <div className="box"></div>
+
+          {/* Text Content */}
+          <div className="text-content">
+            <p
+              className="font-PlayfairDisplaySC font-bold text-4xl lg:text-6xl"
+              data-aos="fade-up"
+              data-aos-duration="1200"
+            >
+              Mern Stack Developer
+            </p>
+            <h1
+              className="text-lg lg:text-2xl font-light mt-4"
+              data-aos="fade-up"
+              data-aos-duration="1500"
+            >
+              Hello and welcome! I'm Mohiuddin Molla, a passionate MERN stack developer based in Germany.
+            </h1>
+            <div className="stack-section mt-10" data-aos="fade-up" data-aos-duration="2000">
+              <h2 className="font-medium text-xl lg:text-2xl">Development Stack</h2>
+              <div className="stack-icons mt-4 lg:w-[600px] md:w-[500px] w-80">
+                <Marquee>
+                  <ul className="flex gap-6 text-2xl lg:text-3xl">
+                    <li className="text-[#E5532D]"><FaHtml5 /></li>
+                    <li className="text-[#2E52E4]"><FaCss3Alt /></li>
+                    <li className="text-[#F7E025]"><TbBrandJavascript /></li>
+                    <li className="text-[#11DAFF]"><FaReact /></li>
+                    <li className="text-[#2E52E4]"><SiTypescript /></li>
+                    <li className="text-[#8FC708]"><DiNodejs /></li>
+                    <li className="text-[#0D3937]"><SiMongodb /></li>
+                    <li className="text-[#8E1AFC]"><SiBootstrap /></li>
+                    <li className="text-[#1DC0CD]"><SiTailwindcss /></li>
+                  </ul>
+                </Marquee>
               </div>
-              </div>
-             
-             
             </div>
           </div>
-          <div className="box hidden lg:flex"></div>
         </div>
       </div>
-      <About />
-      <Work />
-      <Contact />
+      <About></About>
+      <Work></Work>
+      <Contact></Contact>
     </>
   );
 };
